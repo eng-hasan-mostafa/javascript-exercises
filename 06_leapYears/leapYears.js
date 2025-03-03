1158dcfd78cd78531067d948c0736fd701c16de3
Hasan Mostafa <hasanalimostafa1@gmail.com>
2025-03-03 16:30:32 +0300

    Solve 06-leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if(!Number.isInteger(year) || year < 0) return 'ERROR' ;
+    if(year % 4 === 0) {
+        if(year % 100 === 0 && year % 400 ) return false;
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
