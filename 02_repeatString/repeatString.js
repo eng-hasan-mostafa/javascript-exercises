d4d9804d329350ed0fb6b624a9cdbd427167fd22
Hasan Mostafa <hasanalimostafa1@gmail.com>
2025-03-03 13:48:55 +0300

    Solve 02-repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = '';
+    if(num < 0) return 'ERROR';
+    while(num--) {
+        result += string;
+    }
+    return result;
 };
 
 // Do not edit below this line
