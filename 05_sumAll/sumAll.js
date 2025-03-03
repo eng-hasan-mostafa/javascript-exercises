6a1f1a5135eb9c41c732e335172dcec9fa667e22
Hasan Mostafa <hasanalimostafa1@gmail.com>
2025-03-03 16:10:48 +0300

    Solve 05-sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(!Number.isInteger(a) || !Number.isInteger(b)) return 'ERROR';
+    else if(a < 0 || b < 0) return 'ERROR'; 
 
+    let sum = 0;
+    let max = Math.max(a, b);
+    let min = Math.min(a, b);
+    for(let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
