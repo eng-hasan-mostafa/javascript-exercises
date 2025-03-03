33b4dca82c6ba901f81d26c368dfc38c73afd4de
Hasan Mostafa <hasanalimostafa1@gmail.com>
2025-03-03 15:22:40 +0300

    Solve 04-removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...valuesToRemove) {
+    let result = arr.filter((item) => !valuesToRemove.includes(item));
+    return result;
 };
 
 // Do not edit below this line
